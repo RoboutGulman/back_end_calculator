e66cfdddecc9f2bfd8dc981ce0b1609b1a923515
Alex <AlexeyZ213@yandex.ru>
2022-03-24 12:33:45 +0300

    program was divided into functions

--- Program.cs
+++ Program.cs
@@ -13,53 +13,80 @@ namespace Сalculator
             while (true)
             {
                 Console.Clear();
-                double firstValue, secondValue;
-                string action;
-                try
-                {
-                    Console.WriteLine("Введите число 1");
-                    firstValue = double.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Введите число 2");
-                    secondValue = double.Parse(Console.ReadLine());
-                }
-                catch (Exception)
+                string action;
+                var operands = ReadOperands();
+
+                if (operands == null)
                 {
-                    Console.WriteLine("не удалось преобразовать строку в число");
-                    Console.ReadLine();
                     continue;
                 }
 
                 Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
                 action = Console.ReadLine();
 
-                switch (action)
+                var result = ResolveExpression(operands, action);
+
+                if (result != null)
                 {
-                case "+":
-                    Console.WriteLine(firstValue + secondValue);
-                    break;
-                case "-":
-                    Console.WriteLine(firstValue - secondValue);
-                    break;
-                case "*":
-                    Console.WriteLine(firstValue * secondValue);
-                    break;
-                case "/":
-                    if (secondValue == 0)
-                    {
-                        Console.WriteLine("Ошибка! Делить на ноль нельзя");
-                    }
-                    else
-                    {
-                        Console.WriteLine(firstValue / secondValue);
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Ошибка! Неизвестное действие!");
-                    break;
+                    Console.WriteLine(result);
                 }
+
                 Console.ReadLine();
             }
+        }
+
+        private static Tuple<double, double> ReadOperands()
+        {
+            Tuple<double, double> operands;
+            try
+            {
+                Console.WriteLine("Введите число 1");
+                double firstValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите число 2");
+                double secondValue = double.Parse(Console.ReadLine());
+                operands = new Tuple<double, double>(firstValue, secondValue);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("не удалось преобразовать строку в число");
+                Console.ReadLine();
+                return null;
+            }
+            return operands;
+        }
+
+        private static double? ResolveExpression(Tuple<double, double> operands, string action)
+        {
+            double result;
+            switch (action)
+            {
+            case "+":
+                result = operands.Item1 + operands.Item2;
+                break;
+            case "-":
+                result = operands.Item1 - operands.Item2;
+                break;
+            case "*":
+                result = operands.Item1 * operands.Item2;
+                break;
+            case "/":
+                if (operands.Item2 == 0)
+                {
+                    Console.WriteLine("Ошибка! Делить на ноль нельзя");
+                    return null;
+                }
+                else
+                {
+                    result = operands.Item1 / operands.Item2;
+                }
+                break;
+            default:
+                Console.WriteLine("Ошибка! Неизвестное действие!");
+                return null;
+            }
+            return result;
         }
     }
 }
